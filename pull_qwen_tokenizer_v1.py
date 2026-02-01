4ed19c889239a0f973880f2d7d34a8abb26de455
Aamir Mirza <37855753+aamir-gmail@users.noreply.github.com>
2026-02-01 16:17:13 +1100

    Create pull_qwen_tokenizer_v1.py

---
+++ pull_qwen_tokenizer_v1.py
@@ -0,0 +1,24 @@
+# Before you run any code or training, run this script first.
+# this pulls in the tokeniser to your local cache 
+# SDR tokeniser is built on top Qwen tokeniser
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_name = "Qwen/Qwen2.5-0.5B-Instruct"
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype="auto",
+    device_map="auto"
+)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+prompt = "Give me a short introduction to large language model."
+messages = [
+    {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
+    {"role": "user", "content": prompt}
+]
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,
+    add_generation_prompt=True
+)
